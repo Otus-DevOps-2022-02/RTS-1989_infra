11513d62a230269a839be4342bde9dac4339bd1c
RTS-1989 <russibg@gmail.com>
2022-08-24 20:43:23 +0500

    Added s3

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "zone" {
+  description = "Zone"
+  default     = "ru-central1-a"
+}
+variable "cloud_id" {
+  description = "Cloud"
+  default = ""
+}
+variable "folder_id" {
+  description = "Folder"
+}
+
+variable "instance_count" {
+  description = "Amount of instances"
+  default     = 1
+}
+variable "service_account_key_file" {
+  description = "terraform.json"
+}
+variable "bucket_name" {
+  description = "Name of storage"
+  default     = "russi-state-storage"
+}
+variable "access_key" {
+  description = "Key ID"
+}
+variable "secret_key" {
+  description = "secret"
+}
